750c0d945ac9ce32c7c2d4d899a47a6d428834a3
Arunkrishna1998 <arunkvb98@gmail.com>
2023-08-04 19:30:05 +0530

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16.170.227.81']
+ALLOWED_HOSTS = ['13.53.67.159']
 
 
 # Application definition
